07f95d4dce0d8b6b05ed9157ed3256df5919d374
OSunnysun <2088804548@qq.com>
2017-04-06 14:51:40 +0800

    pc
    可以在unity中播放网页视频

---
+++ playMusicOnline/Assets/test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    public string url;
+    public AudioSource source;
+    void Start()
+    {
+        WWW www = new WWW(url);
+        source = GetComponent<AudioSource>();
+        source.clip = www.audioClip;
+    }
+    void Update()
+    {
+        Debug.Log(source.clip);
+        Debug.Log(source.isPlaying);
+        Debug.Log(source.clip.isReadyToPlay);
+        if (!source.isPlaying && source.clip.isReadyToPlay)
+        {
+            source.Play();
+          
+        }
+    }
+}
